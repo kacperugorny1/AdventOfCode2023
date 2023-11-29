4a37cacd7738f677e5f13984d94c1037c5a7ce11
unknown <kacperugorny02@gmail.com>
2023-11-29 09:51:24 +0100

    AoC2023 prepare - input functions

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AdventOfCode2023
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+        static void ReadInputCin()
+        {
+
+            string? line = "";
+            while (true)
+            {
+                line = Console.ReadLine();
+                if (line == "" || line == null || line == "\n")
+                    break;
+                //CODE
+            }
+        }
+
+        static void ReadInputFile()
+        {
+
+            var input = File.ReadLines("FILENAME");
+            foreach(string line in input)
+            {
+                //CODE
+            }
+        }
+    }
+}
