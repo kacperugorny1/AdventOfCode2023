8c2e3848b26dd25fe434392b98c7e24fca295833
unknown <kacperugorny02@gmail.com>
2023-12-04 10:00:36 +0100

    day4 and main cleanup

---
+++ AdventOfCode2023/Day4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day4
+    {
+        static void day4p1(string[] args)
+        {
+            var input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\input.txt");
+            //input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
+            int sum = 0;
+            foreach (string line in input)
+            {
+                int points = 0;
+                List<string> temp;
+                List<string> winning;
+                List<string> yours;
+                temp = line.Split(":").Last().Split("|").ToList();
+                winning = temp.First().Split(" ").ToList();
+                yours = temp.Last().Split(" ").ToList();
+                foreach (string el in yours)
+                {
+                    if (el == "") continue;
+                    if (winning.Contains(el)) points++;
+                }
+                if (points > 0)
+                    points = Convert.ToInt32(Math.Pow(2, points - 1));
+                sum += points;
+            }
+            Console.WriteLine(sum);
+        }
+        static void day4p2(string[] args)
+        {
+            var input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\input.txt");
+            //input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
+            List<int> instances = new() { 0 };
+            for (int i = 0; i < input.Count(); ++i)
+                instances.Add(1);
+            int instance = 0;
+            long sum = 0;
+            foreach (string line in input)
+            {
+                ++instance;
+                int pairs = 0;
+                List<string> temp;
+                List<string> winning;
+                List<string> yours;
+                temp = line.Split(":").Last().Split("|").ToList();
+                winning = temp.First().Split(" ").ToList();
+                yours = temp.Last().Split(" ").ToList();
+                foreach (string el in yours)
+                {
+                    if (el == "") continue;
+                    if (winning.Contains(el)) pairs++;
+                }
+                for (int i = instance + 1; i <= instance + pairs; ++i) instances[i] += instances[instance];
+
+            }
+            foreach (int i in instances) sum += i;
+            Console.WriteLine(sum);
+        }
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace AdventOfCode2023
-{ 
+{
     internal class Program
     {
         // Helpful stuff : Convert.ToInt(), Int32.Parse/TryParse, var val = x switch { 'a' => 2, blbl}, char - '0' = numvalue
@@ -9,45 +9,11 @@ namespace AdventOfCode2023
 
         static void Main(string[] args)
         {
-            List<string> map = new();
-            ulong sum = 0;
             var input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\input.txt");
-            //input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
-
+            input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
             foreach (string line in input)
             {
-                map.Add(line);
             }
-            int start = 0;
-            int end = 0;
-            for(int i = 0; i < map.Count; i++)
-                for(int j = 0; j < map[i].Length; j++)
-                {
-                    ulong num1 = 0;
-                    ulong num2 = 0;
-                    if (map[i][j] != '*')
-                        continue;
-                    for(int x = i - 1; x <= i + 1; ++x){
-                        for(int y = j - 1; y <= j + 1; ++y) {
-                            if (!char.IsNumber(map[x][y])) continue;
-                            start = y;
-                            end = y;
-                            while (start != 0 && char.IsNumber(map[x][start - 1])) start--;
-                            while (end != map[x].Length - 1 && char.IsNumber(map[x][end + 1])) end++;
-                            Console.WriteLine(map[x].Substring(start, end - start + 1));
-                            if (num1 == 0)
-                                num1 = ulong.Parse(map[x].Substring(start, end - start + 1));
-                            else
-                                num2 = ulong.Parse(map[x].Substring(start, end - start + 1));
-                            y = end;
-                        }
-                    }
-                    if (num1 != 0 && num2 != 0)
-                        sum += num1 * num2;
-
-                }
-            Console.WriteLine(sum);
         }
-
     }
 }
