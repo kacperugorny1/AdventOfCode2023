aa155f466073d561417b8515f8e875d1c46a3f27
kacpikoks11 <kacperugorny02@gmail.com>
2023-12-09 11:54:16 +0100

    day8 completed

--- AdventOfCode2023/Day8.cs
+++ AdventOfCode2023/Day8.cs
@@ -42,7 +42,84 @@ namespace AdventOfCode2023
                 }));
                 if (paths[location].place == "ZZZ") break;
             }
+            stopwatch.Stop();
             Console.WriteLine($"Result = {result}");
+            Console.WriteLine($"Elapsed time is {stopwatch.ElapsedMilliseconds} ms");
+        }
+        static void day2p2(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            var input = File.ReadLines("C:\\Users\\Kacpi\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\input.txt");
+            //input = File.ReadLines("C:\\Users\\Kacpi\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
+            long result = 0;
+            List<(string place, string left, string right, int id)> paths = new();
+            List<int> locations = new();
+            List<(long len, long rep)> reps = new();
+            List<(long result, int location)> historyZ = new();
+            string nav = "";
+            stopwatch.Start();
+            foreach (string line in input)
+            {
+                if (line.Length == 0) continue;
+                if (line.Length != 16)
+                {
+                    nav = line;
+                    continue;
+                }
+                if (line[2] == 'A')
+                {
+                    locations.Add(paths.Count());
+                    historyZ.Add(new());
+                    reps.Add((0, 0));
+                }
+                paths.Add((line.Substring(0, 3), line.Substring(7, 3), line.Substring(12, 3), paths.Count));
+            }
+            while (true)
+            {
+                result++;
+                char n = nav[0];
+                nav = nav.Substring(1) + n;
+                for (int i = 0; i < locations.Count(); ++i)
+                {
+                    locations[i] = paths.Find(u =>
+                    {
+                        if (n == 'R') return u.place == paths[locations[i]].right;
+                        else return u.place == paths[locations[i]].left;
+                    }).id;
+                    if (paths[locations[i]].place[2] == 'Z')
+                    {
+                        if (historyZ[i].result == 0)
+                        {
+                            historyZ[i] = (result, locations[i]);
+                        }
+                        else
+                        {
+                            if (reps[i].len == 0)
+                            {
+
+                                Console.WriteLine(result + " " + i);
+                                Console.WriteLine(locations[i]);
+                                if (historyZ[i].location == locations[i])
+                                {
+                                    reps[i] = (historyZ[i].result, result - historyZ[i].result);
+                                    //Console.WriteLine(result);
+                                }
+                            }
+                        }
+
+                    }
+                }
+                if (!reps.Exists(u => u.len == 0)) break;
+            }
+            Console.WriteLine("hello");
+            List<long> len = new(new long[reps.Count]);
+            for (int i = 0; i < reps.Count; i++)
+            {
+                len[i] += reps[i].len;
+                Console.WriteLine(reps[i].rep);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Result = for the result paste it to LCM calculator");
             Console.WriteLine($"Elapsed time is {stopwatch.ElapsedMilliseconds} ms");
         }
     }
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -12,86 +12,38 @@ namespace AdventOfCode2023
         static void Main(string[] args)
         {
             Stopwatch stopwatch = new Stopwatch();
-            var input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\input.txt");
-            //input = File.ReadLines("C:\\Users\\Kacper1\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
+            var input = File.ReadLines("C:\\Users\\Kacpi\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\input.txt");
+            //input = File.ReadLines("C:\\Users\\Kacpi\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
             long result = 0;
-            List<(string place, string left, string right, int id)> paths = new();
-            List<int> locations = new();
-            List<(long len, long rep)> reps = new();
-            List<(long result, int location)> historyZ = new();
-            string nav = "";
             stopwatch.Start();
             foreach (string line in input)
             {
-                if (line.Length == 0) continue;
-                if (line.Length != 16)
+                List<List<int>> hist = new();
+                hist.Add(line.Split(" ").ToList().ConvertAll(x => Int32.Parse(x)));
+                hist.Last().ForEach(u => Console.Write(u + " "));
+                Console.WriteLine();
+                while (true)
                 {
-                    nav = line;
-                    continue;
-                }
-                if (line[2] == 'A')
-                {
-                    locations.Add(paths.Count());
-                    historyZ.Add(new());
-                    reps.Add((0,0));
-                }
-                paths.Add((line.Substring(0, 3), line.Substring(7, 3), line.Substring(12, 3), paths.Count));
-            }
-            while (true)
-            {
-                result++;
-                char n = nav[0];
-                nav = nav.Substring(1) + n;
-                for(int i = 0; i < locations.Count(); ++i)
-                {
-                    locations[i] = paths.Find(u =>
+                    hist.Add(new());
+                    for (int i = 0; i < hist[^2].Count() - 1;++i)
                     {
-                        if (n == 'R') return u.place == paths[locations[i]].right;
-                        else return u.place == paths[locations[i]].left;
-                    }).id;
-                    if (paths[locations[i]].place[2] == 'Z')
-                    {
-                        if (historyZ[i].result == 0)
-                        {
-                            historyZ[i]=(result, locations[i]);
-                        }
-                        else
-                        {
-                            if (reps[i].len == 0)
-                            {
-
-                                Console.WriteLine(result + " " + i);
-                                Console.WriteLine(locations[i]);
-                                if ( historyZ[i].location == locations[i])
-                                {
-                                    reps[i] = (historyZ[i].result, result - historyZ[i].result);
-                                    //Console.WriteLine(result);
-                                }
-                            }
-                        }
-
+                        hist.Last().Add(hist[^2][i + 1] - hist[^2][i]);
+                        Console.Write(hist.Last().Last() + " ");
                     }
+                    Console.WriteLine();
+                    if (hist.Last().All(u => u == 0)) break;
                 }
-                if (!reps.Exists(u => u.len == 0)) break;
-            }
-            Console.WriteLine("hello");
-            List<long> len = new(new long[reps.Count]);
-            for (int i = 0; i < reps.Count; i++)
-            {
-                len[i] += reps[i].len;
-                Console.WriteLine(len[i]);
-                Console.WriteLine(reps[i].rep);
-            }
-            while (true)
-            {
-                for(int i = 0; i < reps.Count; i++)
+                hist.Last().Add(0);
+                for(int i = hist.Count() - 2; i >= 0; --i)
                 {
-                    if (len[i] < len.Max())
-                     len[i] += reps[i].rep;
+                    hist[i].Add(hist[i + 1].Last() + hist[i].Last());
                 }
-                if (len.All(x => x == len[0])) break;
+                result += hist[0].Last();
+
             }
-            result = len[0];
+
+
+            stopwatch.Stop();
             Console.WriteLine($"Result = {result}");
             Console.WriteLine($"Elapsed time is {stopwatch.ElapsedMilliseconds} ms");
         }
