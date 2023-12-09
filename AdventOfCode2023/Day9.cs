1174eda0a3097e1b636a9a8ad0f59b8ff98b5dd7
kacpikoks11 <kacperugorny02@gmail.com>
2023-12-09 11:55:10 +0100

    day9p1

---
+++ AdventOfCode2023/Day9.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day9
+    {
+        static void day9p1(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            var input = File.ReadLines("C:\\Users\\Kacpi\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\input.txt");
+            //input = File.ReadLines("C:\\Users\\Kacpi\\Desktop\\c#\\AdventOfCode2023\\AdventOfCode2023\\inputtest.txt");
+            long result = 0;
+            stopwatch.Start();
+            foreach (string line in input)
+            {
+                List<List<int>> hist = new();
+                hist.Add(line.Split(" ").ToList().ConvertAll(x => Int32.Parse(x)));
+                hist.Last().ForEach(u => Console.Write(u + " "));
+                Console.WriteLine();
+                while (true)
+                {
+                    hist.Add(new());
+                    for (int i = 0; i < hist[^2].Count() - 1; ++i)
+                    {
+                        hist.Last().Add(hist[^2][i + 1] - hist[^2][i]);
+                        Console.Write(hist.Last().Last() + " ");
+                    }
+                    Console.WriteLine();
+                    if (hist.Last().All(u => u == 0)) break;
+                }
+                hist.Last().Add(0);
+                for (int i = hist.Count() - 2; i >= 0; --i)
+                {
+                    hist[i].Add(hist[i + 1].Last() + hist[i].Last());
+                }
+                result += hist[0].Last();
+
+            }
+
+
+            stopwatch.Stop();
+            Console.WriteLine($"Result = {result}");
+            Console.WriteLine($"Elapsed time is {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
